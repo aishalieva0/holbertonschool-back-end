96c36ced28ca921147abf160db70e1376d905c89
aishalieva0 <aysalieva6@gmail.com>
2024-04-23 11:50:24 -0700

    bb

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -21,7 +21,7 @@ def get_todo_progress(user_id):
     
     print(f'Employee {user_name} is done with tasks({num_completed_tasks}/{num_total_tasks}):')
     for task in completed_tasks:
-        print(f'\t{task['title']}')
+        print(f"\t{task['title']}")
 
 
 if __name__ == "__main__":
