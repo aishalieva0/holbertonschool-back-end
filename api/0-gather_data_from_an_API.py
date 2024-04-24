9a3a035a9709702f4c85c9c10b307b9a194458d2
aishalieva0 <aysalieva6@gmail.com>
2024-04-24 03:58:37 -0700

    check int

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -28,7 +28,7 @@ def get_todo_progress(user_id):
 
 
 if __name__ == "__main__":
-    if len(sys.argv) != 2 or sys.argv[1] is not int:
+    if len(sys.argv) != 2:
         exit()
-    id = sys.argv[1]
+    id = int(sys.argv[1])
     get_todo_progress(id)
