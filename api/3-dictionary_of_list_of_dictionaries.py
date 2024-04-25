1a208bba98713c09560ac360ac87649bb01f51a1
aishalieva0 <aysalieva6@gmail.com>
2024-04-25 20:01:05 +0400

    task 3

---
+++ api/3-dictionary_of_list_of_dictionaries.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+"""exports data in the JSON format"""
+
+import json
+import requests
+
+
+def export_to_csv(user_id):
+    base_url = "https://jsonplaceholder.typicode.com"
+    user_endpoint = f"{base_url}/users/"
+
+    user_data = requests.get(user_endpoint).json()
+
+    all_tasks = {}
+    for user in user_data:
+        user_id = user["id"]
+        user_name = user["username"]
+
+        todo_endpoint = f"{base_url}/todos?userId={user_id}"
+        todos_data = requests.get(todo_endpoint).json()
+
+        tasks = [
+            {
+                "username": user_name,
+                "task": task["title"],
+                "completed": task["completed"],
+            }
+            for task in todos_data
+        ]
+
+        all_tasks[user_id] = tasks
+
+    with open("todo_all_employees.json", "w") as f:
+        json.dump(all_tasks, f)
+
+
+if __name__ == "__main__":
+    export_to_csv(id)
